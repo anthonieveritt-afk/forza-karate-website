13df8613c7f3e04c63717e21c69526256ff07605
anthonieveritt-afk <anthonieveritt@gmail.com>
2026-05-20 22:39:26 +0100

    Add photo to home hero rotation (hero17)

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -28,6 +28,7 @@ const photos = [
   { src: '/team/team-competition-01.jpg',   alt: 'Forza Karate competition',           position: '50% 25%' },
   { src: '/hero15.jpg',                     alt: 'Forza Karate sparring',              position: '50% 25%' },
   { src: '/hero16.jpg',                     alt: 'Forza Karate',                       position: '50% 25%' },
+  { src: '/hero17.jpg',                     alt: 'Forza Karate',                       position: '50% 25%' },
 ]
 
 export default function Hero() {
