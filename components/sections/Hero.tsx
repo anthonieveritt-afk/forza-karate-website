62f788872ebdcb7e7e7aeded7bd2f795eb970b9b
anthonieveritt-afk <anthonieveritt@gmail.com>
2026-05-20 22:33:38 +0100

    Add flipped sparring photo to home hero rotation (hero15)

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -26,6 +26,7 @@ const photos = [
   { src: '/team/team-05.jpg',               alt: 'Forza Karate squad',                 position: '50% 25%' },
   { src: '/team/team-group.jpg',            alt: 'Forza Karate squad group',           position: '50% 25%' },
   { src: '/team/team-competition-01.jpg',   alt: 'Forza Karate competition',           position: '50% 25%' },
+  { src: '/hero15.jpg',                     alt: 'Forza Karate sparring',              position: '50% 25%' },
 ]
 
 export default function Hero() {
