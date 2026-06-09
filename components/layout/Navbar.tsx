2397447cded1c20fce8ad1682d43fe7c8f5420ad
anthonieveritt-afk <anthonieveritt@gmail.com>
2026-06-09 15:48:13 +0100

    fix: force redeploy to pick up CLUB_HONBU_WEBHOOK_SECRET env var

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -35,10 +35,10 @@ export default function Navbar() {
   return (
     <header className="sticky top-0 z-50 bg-white border-b border-black/8">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex items-center justify-between h-16">
+        <div className="flex items-center justify-between py-1">
           {/* Logo */}
           <Link href="/" className="flex items-center">
-            <Image src="/forza-logo.webp" alt="Forza Karate Club" width={120} height={48} className="h-10 w-auto object-contain" priority />
+            <Image src="/forza-logo.webp" alt="Forza Karate Club" width={200} height={80} className="h-[45px] md:h-[80px] w-auto object-contain" priority />
           </Link>
 
           {/* Desktop nav */}
